30880114d6fcce060b45c1a6d585d19802cb6a67
leofontolan <leofontolan@gmail.com>
2022-04-25 21:33:27 -0300

    Entidade DDD

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,21 +1,35 @@
 class Customer{
 
-    _id: string;
-    _name: string;
-    _address: string;
-    _active: boolean = true;
+    _id: string = "";
+    _name: string = "";
+    _address: string = "";
+    _active: boolean = false;
 
-    constructor(id: string, name: string, address: string){
+    constructor(id: string, name: string){
         this._id = id;
         this._name = name;
-        this._address = address;
+        this.validate();
+    }
+
+
+    validate(){
+        if (this._name.length === 0){
+            throw new Error("Name is required");
+        }
+        if (this._id.length === 0){
+            throw new Error("Id is required");
+        }
     }
 
    changeName(name: string){
         this._name = name;
+        this.validate();
     }
 
     activate(){
+        if(this._address.length === 0){
+            throw new Error("Address is required");
+        }
         this._active = true;
     }
 
@@ -23,3 +37,5 @@ class Customer{
         this._active = false;
     }
 }
+
+let customer = new Customer("123", "");
