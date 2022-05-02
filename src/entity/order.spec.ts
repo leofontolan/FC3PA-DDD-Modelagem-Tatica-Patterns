2aaa3ed93bd89d120a1c6a23382f221d6af87106
leofontolan <leofontolan@gmail.com>
2022-05-02 20:50:49 -0300

    Implementando quantidade em Products e Test

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -22,15 +22,15 @@ describe("Order unit Test", () => {
     });
 
     it("should calculate total", () => {
-       const item1 = new OrderItem("i1", "Item 1", 100);
-       const item2 = new OrderItem("i2", "Item 2", 200);
+       const item1 = new OrderItem("i1", "Item 1", 100, "p1", 2);
+       const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
        const order = new Order("o1", "c1", [item1]);
        let total = order.total();
 
-       expect(total).toBe(100);
+       expect(total).toBe(200);
 
        const order2 = new Order("o2", "c2", [item1, item2]);
        total = order2.total();
-       expect(total).toBe(300);
+       expect(total).toBe(600);
     });
 });
