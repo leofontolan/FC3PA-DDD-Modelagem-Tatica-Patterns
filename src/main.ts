b3437f5ba5eb13a082ea884c1e5186395e6bcff1
leofontolan <leofontolan@gmail.com>
2022-05-02 21:35:06 -0300

    Adicionando a verificação do TSC

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const address = new Address("Rua do Leão", 2, "12345-678","São Paulo");
 customer.address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Coca-Cola", 5.5);
-const item2 = new OrderItem("2", "Temaki", 15);
+const item1 = new OrderItem("1", "Coca-Cola", 5.5, "p1", 2);
+const item2 = new OrderItem("2", "Temaki", 15, "p2", 2);
 
 const order = new Order("1", "123", [item1, item2]);
