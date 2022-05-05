db950325f889b546dc7265be7ceea2d0e54e4767
leofontolan <leofontolan@gmail.com>
2022-05-05 20:19:39 -0300

    Testando Rewards

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,6 +1,7 @@
 import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
+import {v4 as uuid} from "uuid";
 
 export default class OrderService{
     
@@ -10,7 +11,7 @@ export default class OrderService{
             throw new Error("Order must have at least one item");
         }
 
-        const order = new Order("124", customer.id, items);
+        const order = new Order(uuid(), customer.id, items);
         customer.addRewardPoints(order.total()/2);
         return order;
     }
