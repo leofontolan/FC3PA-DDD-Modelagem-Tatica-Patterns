5ac0a1e3a7c4f82ecd8199f27807827979e46c7b
leofontolan <leofontolan@gmail.com>
2022-05-05 20:11:10 -0300

    Implementado Rewards

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -6,6 +6,7 @@ export default class Customer{
     private _name: string = "";
     private _address!: Address
     private _active: boolean = false;
+    private _rewardPoints: number = 0;
 
     constructor(id: string, name: string){
         this._id = id;
@@ -13,8 +14,17 @@ export default class Customer{
         this.validate();
     }
 
+    get id(): string{
+        return this._id;
+    }
+
     get name(): string{
         return this._name;
+    }
+
+
+    get rewardPoints(): number{
+        return this._rewardPoints;
     }
 
     validate(){
@@ -46,6 +56,11 @@ export default class Customer{
     deactivate(){
         this._active = false;
     }
+
+    addRewardPoints(points: number){
+        this._rewardPoints += points;
+    }
+
 
     set address(address: Address){
         this._address = address;
