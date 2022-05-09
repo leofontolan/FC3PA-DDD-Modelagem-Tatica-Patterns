71d58e9e7ed2c1150b6fb1d965606c37350a0c50
leofontolan <leofontolan@gmail.com>
2022-05-09 20:04:34 -0300

    Ajustando tsconfig para trabalhar com banco

---
+++ src/domain/infrastruture/db/sequelize/model/product.model.ts
@@ -0,0 +1,13 @@
+import { Column, Model, PrimaryKey, Table } from "sequelize-typescript";
+
+@Table({
+    tableName: "products",
+    timestamps: false,
+})
+
+export default class ProductModel extends Model {
+
+    @PrimaryKey
+    @Column
+    declare id: any;
+}
