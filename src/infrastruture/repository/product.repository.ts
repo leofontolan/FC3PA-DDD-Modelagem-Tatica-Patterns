36ccb14e9c8bc821910ab854b650d24df89794c7
leofontolan <leofontolan@gmail.com>
2022-05-10 20:22:26 -0300

    ImImplementando metodo Updade

--- src/infrastruture/repository/product.repository.ts
+++ src/infrastruture/repository/product.repository.ts
@@ -1,15 +1,26 @@
 import Product from "../../domain/entity/product";
 import ProductRepositoryInterface from "../../domain/repository/product-repository.interface";
+import ProductModel from "../db/sequelize/model/product.model";
 
 
 export default class ProductRepository implements ProductRepositoryInterface {
 
     async create(entity: Product): Promise<void> {
-        throw new Error("Method not implemented.");
+        
+        await ProductModel.create({
+            id: entity.id,
+            name: entity.name,
+            price: entity.price,
+        });
     }
 
     async update(entity: Product): Promise<void> {
-        throw new Error("Method not implemented.");
+        
+        await ProductModel.update({
+            name: entity.name,
+            price: entity.price,
+        }, 
+        { where: { id: entity.id } });
     }
 
     async find(id: number): Promise<Product> {
